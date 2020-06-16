0c5a38573a0c3d892ee738f3703137044ca6e475
aldohmolina <aldohmolina@gmail.com>
2020-06-16 10:25:15 -0500

    modulo cors

--- serverless/api/index.js
+++ serverless/api/index.js
@@ -1,27 +1,22 @@
+// node_modules
 const express = require('express')
 const mongoose = require('mongoose')
 const bodyParset = require('body-parser')
+const cors = require('cors')
+
+// models
 const meals = require('./routers/meals')
 const orders = require('./routers/orders')
+
+//app use
 const app = express()
 app.use(bodyParset.json())
+app.use(cors())
 
 mongoose.connect(process.env.MONGODB_URI_SERVERLESS, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
-
-// const Users = mongoose.model(
-//     'User',new mongoose.Schema({
-//     name:'String'
-// }))
-// Users.create({name:'chanchito triste'})
-
-
-// app.get('*',(req,res) => {
-//     Users.find()
-//     .then(x => res.send(x))
-// })
 
 app.use('/api/meals',meals)
 app.use('/api/orders',orders)
